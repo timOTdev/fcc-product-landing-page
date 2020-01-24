ee8457dc34ac872684f102392ed95ebdc4b987be
Timothy H <timh1203@gmail.com>
2020-01-24 12:14:14 -0600

    Add working carousel with logic

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -1,12 +1,68 @@
+// PACKAGE IMPORTS
+import React from 'react'
+
 // LOCAL IMPORTS
 import './Carousel.scss'
 
-const Carousel = () => {
+const sources = [
+  '/static/hero1.jpg',
+  '/static/hero2.jpg',
+]
+
+const Slide = ({ url }: any) => {
+  const styles = {
+    backgroundImage: `url(${url})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '970px',
+    height: '300px',
+    borderRadius: '3px',
+    border: '10px solid lightgray',
+  };
   return (
-    <main id="carousel">
-      <img className="carousel-1" src="/static/hero1.jpg" alt="Feature Banner" />
-      <img className="carousel-2" src="/static/hero2.jpg" alt="Hero Banner" />
-    </main>
+    <div className="image-slide" style={styles}></div>
   )
 }
+
+const Arrow = ({ direction, clickFunction, glyph }: any) => (
+  <div
+    className={`arrow ${direction}`}
+    onClick={clickFunction}>
+    {glyph}
+  </div>
+);
+
+class Carousel extends React.Component {
+  state = {
+    currentIndex: 0,
+  }
+
+  prev = () => {
+    if (this.state.currentIndex === 0) {
+      this.setState({ currentIndex: sources.length - 1 })
+    } else {
+      this.setState({ currentIndex: this.state.currentIndex - 1 })
+    }
+  }
+
+  next = () => {
+    if (this.state.currentIndex >= sources.length - 1) {
+      this.setState({ currentIndex: 0 })
+    } else {
+      this.setState({ currentIndex: this.state.currentIndex + 1 })
+    }
+  }
+
+  render() {
+    const { currentIndex } = this.state
+    return (
+      <main id="carousel">
+        <Arrow direction="left" clickFunction={this.prev} glyph="&#9664;" />
+        <Slide url={sources[currentIndex]} />
+        <Arrow direction="right" clickFunction={this.next} glyph="&#9654;" />
+      </main>
+    )
+  }
+}
+
 export default Carousel
