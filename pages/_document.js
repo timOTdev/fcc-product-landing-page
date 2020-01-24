b0c9708917d0f2b8021dcd20a508cfda3edf6be7
Timothy H <timh1203@gmail.com>
2020-01-24 12:36:20 -0600

    Adding google fonts and font stylings

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+class CustomDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <link href="https://fonts.googleapis.com/css?family=Cousine|Playfair+Display&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default CustomDocument;
